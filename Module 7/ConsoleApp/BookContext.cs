92bc68f153acbe9365a33ddbc2d904388c2c9dbe
Skewnart <corentinz@outlook.fr>
2025-04-16 17:41:50 +0200

    feat: Module 7

---
+++ Module 7/ConsoleApp/BookContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Auteur> Authors { get; set; }
+        public DbSet<Livre> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\repos\\perso\\Supinfo-2025-3ASP-private\\Module 7\\ConsoleApp\\Database1.mdf\";Integrated Security=True");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 - [Module 4 - POO en C# partie 1](https://github.com/Skewnart/Supinfo-2025-3ASP/tree/main/Module%204)
 - [Module 5 - POO en C# partie 2](https://github.com/Skewnart/Supinfo-2025-3ASP/tree/main/Module%205)
 - [Module 6 - Principes avancées](https://github.com/Skewnart/Supinfo-2025-3ASP/tree/main/Module%206)
+- [Module 7 - Les données](https://github.com/Skewnart/Supinfo-2025-3ASP/tree/main/Module%207)
